010f84e97e5bb82b7d1d715b2a3a41900ae037c9
Russell Ramey <doublercreative@gmail.com>
2022-06-06 12:37:23 -0500

    code refactor/cleanup

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.use(express.json());
  * @param String
  * @return Object | Boolean
  */
-const toJson = function(str){
-    // If no data 
+const strToJson = (str) => {
+    // Arguments exists and valid
     if(!str) return false;
     // Try to convert string to json
     try{
@@ -49,61 +49,15 @@ const toJson = function(str){
     }
 }
 /**
- * Create columns array
- * Sperate and prepare column values from data object
- * @param Object
- * @return Array | Boolean
- */
-const parseColumns = function(data){
-    // If no data 
-    if(!data || !data.table) return false;
-    // Column array
-    let cols;
-    // If parsed headers
-    if(data.table.parsedNumHeaders){
-        cols = data.table.cols.map((col, index) => {
-            return (col.label) ? col.label.replace(/ /g, '_').replace(/-/g, '_') : col.id
-        });
-    } else {
-        // If no rows array
-        if(!data.table.rows) return false;
-        // Map first element of rows array as columns
-        cols = data.table.rows[0].c.map((col, index) => {
-            return (col.v) ? col.v.replace(/ /g, '_').replace(/-/g, '_') : index
-        });
-    }
-    // Return cols array
-    return cols; 
-}
-/**
- * Parse rows array
- * Sperate and prepare row values from data object
- * @param Object
- * @return Array | Boolean
- */
-const parseRows = function(data){
-    // If no data 
-    if(!data || !data.table) return false;
-    // Parse rows to new array
-    let rows = data.table.rows.map((row) => row.c );
-    // If no parsed headers from data
-    if(!data.table.parsedNumHeaders){
-        // Remove first element in rows array
-        rows.shift();
-    }
-    // Return rows array
-    return rows;
-}
-/**
  * Create JSON
  * Create new json object using rows and columns array
  * Columns as json keys, Rows as json values
  * @param Array - cols 
  * @param Array - rows
- * @return Array | Boolean
+ * @return Array
  */
-const createJson = function(cols, rows){
-    // If missing arguments 
+ const createJson = (cols, rows) => {
+    // Arguments exists and valid
     if(!cols || !rows) return false;
     // new array 
     let arr = [];
@@ -125,8 +79,55 @@ const createJson = function(cols, rows){
         // Push to json
         arr.push(obj)
     })
-    // Return data
+    // Return array
     return arr;
+}
+/**
+ * Create columns array
+ * Sperate and prepare column values from data object
+ * @param Object
+ * @return Array | Boolean
+ */
+const parseColumns = (data) => {
+    // Arguments exists and valid
+    if(!data || !data.table) return false;
+    // Column array
+    let cols;
+    // If parsed headers
+    if(data.table.parsedNumHeaders){
+        // Map table columns to new array
+        cols = data.table.cols.map((col, index) => {
+            return (col.label) ? col.label.replace(/ /g, '_').replace(/-/g, '_') : col.id
+        });
+    } else {
+        // If no rows array
+        if(!data.table.rows) return false;
+        // Map first element of table rows array to new array
+        cols = data.table.rows[0].c.map((col, index) => {
+            return (col.v) ? col.v.replace(/ /g, '_').replace(/-/g, '_') : index
+        });
+    }
+    // Return cols array
+    return cols; 
+}
+/**
+ * Parse rows array
+ * Sperate and prepare row values from data object
+ * @param Object
+ * @return Array | Boolean
+ */
+const parseRows = (data) => {
+    // Arguments exists and valid
+    if(!data || !data.table) return false;
+    // Parse rows to new array
+    let rows = data.table.rows.map((row) => row.c );
+    // If no parsed headers from data
+    if(!data.table.parsedNumHeaders){
+        // Remove first element in rows array
+        rows.shift();
+    }
+    // Return rows array
+    return rows;
 }
 /**
  * Parse Data
@@ -134,17 +135,17 @@ const createJson = function(cols, rows){
  * @param String - data 
  * @return JSON
  */
-const parseData = function(data){
-    // If no data 
+const parseData = (data) => {
+    // Arguments exists and valid
     if(!data) return false;
     // Try to parse data
     try{
         // Split sting at new lines
-        data = toJson(data);
+        data = strToJson(data);
         // Get seperate arrays for columsn/rows
         cols = parseColumns(data);
         rows = parseRows(data);
-        // Return data
+        // Return new json object
         return createJson(cols, rows);        
     } 
     // Catch any errors
@@ -156,28 +157,24 @@ const parseData = function(data){
 /**
  * Sort data
  * Sort data array based on provided params
- * @param Object - obj
+ * @param Array - data
  * @param String - key
  * @param Boolean - reverse 
  * @return Object
  */
-const sortJson = function(obj, key, reverse=false){
-    // If key exists
+const sortData = (data, key, reverse=false) => {
     if(key){
        // Sort object by key 
-        obj.sort((a, b) => {
+       data.sort((a, b) => {
             if(a[key] > b[key]) return 1;
             if(a[key] < b[key]) return -1;
             return 0;
         })
     }
     // If reverse is true
-    if(reverse){
-        // Reverse object order
-        obj.reverse();
-    }
-    // Return object
-    return obj;
+    if(reverse) data.reverse();
+    // Return data
+    return data;
 }
 
 /************************************
@@ -185,7 +182,7 @@ const sortJson = function(obj, key, reverse=false){
 * App Routes
 *
 ************************************/
-app.get("/json/:sheetid/", (request, response, next) => {
+app.get("/doc/:sheetid/", (request, response, next) => {
     
     // If no sheet id
     if(!request.params.sheetid ) return response.status(400).json({ error: true, message: 'Valid publicly accessible Sheet ID required.' });
@@ -222,14 +219,11 @@ app.get("/json/:sheetid/", (request, response, next) => {
                 if(request.query.order === 'asc') reverse = false;
                 if(request.query.order === 'desc') reverse = true;
                 // Sort data based off params
-                data = sortJson(data, request.query.orderby, reverse);
+                data = sortData(data, request.query.orderby, reverse);
             }
 
-            // If data has error
-            if(data.error) return response.status(400).json(data);
-
             // Return new respones
-            return response.status(200).json(data);
+            return response.status((!data.error ? 200 : 400)).json(data);
 
         });
       
